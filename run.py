756f038714dc18ceead80b83b0f6489d363dfd20
Emma Scott <emmajane1276@gmail.com>
2022-11-20 21:21:23 +0000

    challenge

--- run.py
+++ run.py
@@ -135,6 +135,7 @@ def main():
 print("Welcome to Love Sandwiches Data Automation")
 main()
 
+
 def get_stock_values(data):
     """
     Print out the calculated stock numbers for each sandwich type.
@@ -147,5 +148,7 @@ def get_stock_values(data):
     #     new_data[heading] = stock_num
     # return new_data
     return {heading: data for heading, data in zip(headings, data)}
+
+
 stock_values = get_stock_values(stock_data)
 print(stock_values)
